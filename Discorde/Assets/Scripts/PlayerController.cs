bba1bd04fcb8564440c2344b3d60312792280aad
Cybermiam <gaeldimalta@gmail.com>
2024-12-12 00:15:42 +0100

    added battlefield

---
+++ Discorde/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+    public int jumpForce = 5;
+
+    public int bumpForce = 5;
+
+    public Boolean touchingGrass = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.speed = 1500f;
+        this.jumpForce = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && touchingGrass)
+        {   
+            touchingGrass = false;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ground"){
+            touchingGrass = true;
+        } else if (collision.gameObject.name == "AISphere")
+        {
+            Rigidbody aiSphereRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (aiSphereRb != null && aiSphereRb.linearVelocity.magnitude < GetComponent<Rigidbody>().linearVelocity.magnitude)
+            {
+                
+                bumpForce += 10;
+                transform.localScale *= 1.1f;
+            } else{
+                this.speed += 500f;
+                
+            }
+            aiSphereRb.AddForce(-collision.contacts[0].normal * bumpForce, ForceMode.Impulse);
+        }
+    }
+}
